1b19cc079c8e96797f27679a57d07c49d1fa0778
Mac <mac@abctech-thailand.com>
2013-12-23 16:52:32 +0700

    Implement auth page

--- ripoti-webapp/src/main/java/com/abctech/ripoti/webapp/controller/MainController.java
+++ ripoti-webapp/src/main/java/com/abctech/ripoti/webapp/controller/MainController.java
@@ -1,13 +1,29 @@
 package com.abctech.ripoti.webapp.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class MainController {
 
+    private static final Logger log = LoggerFactory.getLogger(MainController.class);
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping(value = {"/", "/index"})
     public String index() {
-        return "index";
+        return "redirect:auth";
+    }
+
+    @RequestMapping(value = "auth")
+    public String auth(Model model) {
+        model.addAttribute("pageContent", "main/auth");
+        return "layout";
     }
 }
---
+++ ripoti-webapp/src/main/webapp/WEB-INF/jsp/layout.jsp
@@ -0,0 +1,11 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Ripoti</title>
+    </head>
+    <body>
+        <jsp:include page="${pageContent}.jsp" />
+    </body>
+</html>
