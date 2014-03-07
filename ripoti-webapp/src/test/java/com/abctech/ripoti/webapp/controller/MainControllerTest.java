9801385595fa957797888d20d823d5aa14ec2427
Mac <mac@abctech-thailand.com>
2014-03-07 17:12:54 +0700

    Add unit test for MainController

--- ripoti-webapp/src/test/java/com/abctech/ripoti/webapp/controller/MainControllerTest.java
+++ ripoti-webapp/src/test/java/com/abctech/ripoti/webapp/controller/MainControllerTest.java
@@ -1,9 +1,16 @@
 package com.abctech.ripoti.webapp.controller;
 
+import com.abctech.ripoti.webapp.json.jira.JiraSession;
+import com.abctech.ripoti.webapp.json.jira.rapidview.View;
+import com.abctech.ripoti.webapp.json.jira.search.Issue;
+import com.abctech.ripoti.webapp.json.jira.search.Search;
+import com.abctech.ripoti.webapp.json.jira.sprintquery.Sprint;
+import com.abctech.ripoti.webapp.json.ripoti.RipotiIssue;
 import com.abctech.ripoti.webapp.service.IJiraAuthStorageService;
 import com.abctech.ripoti.webapp.service.JiraRestService;
 import com.abctech.ripoti.webapp.service.JiraToRipotiService;
 import com.abctech.ripoti.webapp.service.JiraViewStorageService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,10 +21,16 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.client.HttpClientErrorException;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MainControllerTest {
@@ -66,19 +79,139 @@ public class MainControllerTest {
     public void testAuthNotLoggedIn() throws Exception {
         when(jiraAuthStorageService.getAuthorizationValue()).thenReturn(null);
         mockMvc.perform(MockMvcRequestBuilders.get("/auth"))
-                .andExpect(model().attribute("pageContent", "main/auth"));
+                .andExpect(model().attribute("pageContent", "main/auth")).andExpect(view()
+                .name("layout"));
     }
 
-//    @Test
-//    public void testAuthLoginSuccess() throws Exception {
-//        when(jiraAuthStorageService.getAuthorizationValue()).thenReturn(null);
-//        JiraSession jiraSession = new JiraSession();
-//        jiraSession.setName("name");
-//        when(jiraRestService.getJiraSession(anyString())).thenReturn(jiraSession);
-//        when(jiraAuthStorageService.getAuthorizationValue()).thenReturn("")
-//        mockMvc.perform(MockMvcRequestBuilders.post("/auth")
-//                .requestAttr("username", "myuser")
-//                .requestAttr("password", "mypass"))
-//                .andExpect(model().attribute("pageContent", "main/auth"));
-//    }
+    @Test
+    public void testAuthLoginSuccess() throws Exception {
+        when(jiraAuthStorageService.getAuthorizationValue()).thenReturn(null, "123");
+        JiraSession jiraSession = new JiraSession();
+        jiraSession.setName("name");
+        when(jiraRestService.getJiraSession(anyString())).thenReturn(jiraSession);
+        mockMvc.perform(MockMvcRequestBuilders.post("/auth")
+                .requestAttr("username", "myuser")
+                .requestAttr("password", "mypass"))
+                .andExpect(model().attribute("pageContent", "main/auth"))
+                .andExpect(view().name("redirect:report"));
+    }
+
+    @Test
+    public void testAuthLoginFail() throws Exception {
+        when(jiraAuthStorageService.getAuthorizationValue()).thenReturn(null);
+        JiraSession jiraSession = new JiraSession();
+        jiraSession.setName("name");
+        when(jiraRestService.getJiraSession(anyString())).thenThrow(HttpClientErrorException.class);
+        mockMvc.perform(MockMvcRequestBuilders.post("/auth")
+                .requestAttr("username", "myuser")
+                .requestAttr("password", "mypass"))
+                .andExpect(model().attribute("pageContent", "main/auth"))
+                .andExpect(model().attributeExists("errorMsg"));
+    }
+
+    @Test
+    public void testReport() throws Exception {
+        when(jiraAuthStorageService.getAuthorizationValue()).thenReturn("123");
+        View[] views = new View[2];
+        View view = new View();
+        view.setId(1);
+        view.setName("Board 1");
+        views[0] = view;
+        view = new View();
+        view.setId(2);
+        view.setName("Board 2");
+        views[1] = view;
+        when(jiraViewStorageService.load()).thenReturn(views);
+        Sprint[] sprints = new Sprint[3];
+        Sprint sprint = new Sprint();
+        sprint.setId(1);
+        sprint.setName("Sprint 1");
+        sprints[0] = sprint;
+        sprint = new Sprint();
+        sprint.setId(2);
+        sprint.setName("Sprint 2");
+        sprints[1] = sprint;
+        sprint = new Sprint();
+        sprint.setId(3);
+        sprint.setName("Sprint 3");
+        sprints[2] = sprint;
+        when(jiraRestService.getSprints(anyString(), anyInt())).thenReturn(sprints);
+        when(jiraRestService.getSearch(anyString(), anyInt())).thenReturn(new Search());
+        when(jiraToRipotiService.convert(any(Issue[].class))).thenReturn(new RipotiIssue());
+        when(jacksonObjectMapper.writeValueAsString(any(RipotiIssue.class)))
+                .thenReturn("{test:\"test\"}");
+        mockMvc.perform(MockMvcRequestBuilders.get("/report")
+                .param("viewId", "1")
+                .param("sprintId", "1"))
+                .andExpect(model().attribute("pageContent", "main/report"))
+                .andExpect(model().attributeExists("viewMap"))
+                .andExpect(model().attributeExists("sprintMap"))
+                .andExpect(model().attribute("viewName", "Board 1"))
+                .andExpect(model().attribute("sprintName", "Sprint 1"))
+                .andExpect(model().attributeExists("ripotiJson"))
+                .andExpect(view().name("layout"));
+        verify(jacksonObjectMapper).writeValueAsString(any(RipotiIssue.class));
+    }
+
+    @Test
+    public void testReportErrorJsonProcessing() throws Exception {
+        when(jiraAuthStorageService.getAuthorizationValue()).thenReturn("123");
+        View[] views = new View[2];
+        View view = new View();
+        view.setId(1);
+        view.setName("Board 1");
+        views[0] = view;
+        view = new View();
+        view.setId(2);
+        view.setName("Board 2");
+        views[1] = view;
+        when(jiraViewStorageService.load()).thenReturn(views);
+        Sprint[] sprints = new Sprint[3];
+        Sprint sprint = new Sprint();
+        sprint.setId(1);
+        sprint.setName("Sprint 1");
+        sprints[0] = sprint;
+        sprint = new Sprint();
+        sprint.setId(2);
+        sprint.setName("Sprint 2");
+        sprints[1] = sprint;
+        sprint = new Sprint();
+        sprint.setId(3);
+        sprint.setName("Sprint 3");
+        sprints[2] = sprint;
+        when(jiraRestService.getSprints(anyString(), anyInt())).thenReturn(sprints);
+        when(jiraRestService.getSearch(anyString(), anyInt())).thenReturn(new Search());
+        when(jiraToRipotiService.convert(any(Issue[].class))).thenReturn(new RipotiIssue());
+        when(jacksonObjectMapper.writeValueAsString(any(RipotiIssue.class)))
+                .thenThrow(JsonProcessingException.class);
+        mockMvc.perform(MockMvcRequestBuilders.get("/report")
+                .param("viewId", "1")
+                .param("sprintId", "1"))
+                .andExpect(model().attribute("pageContent", "main/report"))
+                .andExpect(model().attributeExists("viewMap"))
+                .andExpect(model().attributeExists("sprintMap"))
+                .andExpect(model().attribute("viewName", "Board 1"))
+                .andExpect(model().attribute("sprintName", "Sprint 1"))
+                .andExpect(model().attributeDoesNotExist("ripotiJson"))
+                .andExpect(view().name("layout"));
+        verify(jacksonObjectMapper).writeValueAsString(any(RipotiIssue.class));
+    }
+
+    @Test
+    public void testUpdateBoard() throws Exception {
+        when(jiraAuthStorageService.getAuthorizationValue()).thenReturn("authvalue");
+        View[] views = new View[2];
+        View view = new View();
+        view.setId(1);
+        view.setName("Board 1");
+        views[0] = view;
+        view = new View();
+        view.setId(2);
+        view.setName("Board 2");
+        views[1] = view;
+        when(jiraRestService.getViews(anyString())).thenReturn(views);
+        mockMvc.perform(MockMvcRequestBuilders.get("/updateboard"))
+                .andExpect(redirectedUrl("report"));
+        verify(jiraViewStorageService).save(any(View[].class));
+    }
 }
